9b785b9a2a0bff8d3b638348f9ac8184e282a784
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-06-05 09:46:30 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    
+    let sum = 0;
+    
+    for(let i = start; i <= end; i++ ){
+        sum += i;
+    }
+    
+    return sum;
+}
