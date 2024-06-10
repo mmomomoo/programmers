bb96040cf532bf7fa2866c7aa6915b39222332e0
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-06-10 21:02:12 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,9 +1,1 @@
-function solution(num1, num2) {
-    let answer = 0;
-    if(num1 === num2) {
-        return 1;
-    } else {
-        return -1;
-    }
-    return answer;
-}
+const solution = (num1, num2) => (num1 === num2)? 1 : -1; 
