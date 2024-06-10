7bb0b067daf694071922faf21a8f34a3053e32c8
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-06-10 22:25:23 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,4 +1,8 @@
-function solution(angle) {
-    var answer = 0;
-    return answer;
+const solution = (angle) => {
+    return (0 < angle && angle < 90) ? 1 :
+           (angle === 90)? 2:
+           (90 < angle && angle < 180)? 3: 
+           (angle === 180 )? 4 : 0;
 }
+
+//0은 예외처리로 
