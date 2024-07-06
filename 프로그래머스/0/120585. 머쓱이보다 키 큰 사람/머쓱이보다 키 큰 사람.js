4764197fa9a8c20944d91b395d8cd8441c983e50
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-07-06 20:46:03 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,8 +1,8 @@
 function solution(array, height) {
     let count = 0;
-    for (let i = 0; i < array.length; i ++) {
-        if (array[i] > height) {
-            count++;
+    for(i = 0; i < array.length; i++){
+        if(array[i] > height){
+            count++
         }
     }
     return count;
