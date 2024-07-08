7a192f143f9642db6e48503644aeaa1178ba8dd6
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-07-08 15:04:19 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,3 +1,11 @@
 function solution(hp) {
-    return Math.floor(hp / 5) + Math.floor(hp % 5 / 3) + (hp % 5 % 3);
+   const generalAnt = Math.floor(hp / 5);
+    hp = hp % 5;
+   const soldierAnt = Math.floor(hp / 3);
+    hp = hp % 3;
+   const privateAnts = Math.floor(hp / 1);
+    hp = hp % 1;
+    
+    const totalAnt = generalAnt + soldierAnt + privateAnts;
+    return totalAnt;
 }
