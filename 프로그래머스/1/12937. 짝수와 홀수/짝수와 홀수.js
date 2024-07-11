f9b807a2e1390117ac0c089b2e3e81b12d503d4f
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-07-11 09:59:04 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,6 +1,4 @@
-function solution(num) {
-    if (num === 0 ||num % 2 === 0){
-        return'Even'
-    } else {
-        return 'Odd'
-    }}
+const solution = num => {
+   return num % 2 === 0? 'Even' : 'Odd'
+  
+}
