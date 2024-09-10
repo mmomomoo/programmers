96ed5a28106c58c59389adfa5df94dac181d3a0b
momomoo <164153608+mmomomoo@users.noreply.github.com>
2024-09-10 16:32:32 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,11 +1,5 @@
-const solution = my_string => {
-    let newstring = '';
-    for(i = 0; i < my_string.length; i++){
-    if(my_string[i] === my_string[i].toUpperCase()){
-        newstring += my_string[i].toLowerCase();
-    } else if (my_string[i] === my_string[i].toLowerCase()) {
-        newstring += my_string[i].toUpperCase();
-    }
-  }
-    return newstring;
+function solution(my_string) {
+    let result = '';
+    for (let char of my_string) result += char === char.toLowerCase()?char.toUpperCase():char.toLowerCase()
+    return result;
 }
