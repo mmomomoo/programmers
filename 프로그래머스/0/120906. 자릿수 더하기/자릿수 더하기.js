291105e7818bb9e5bbff86c0634784d96665af6a
momomoo <164153608+mmomomoo@users.noreply.github.com>
2024-09-19 14:49:29 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,9 +1,3 @@
 function solution(n) {
-    let sum = 0;
-    while (n > 0) {
-       sum += n % 10;
-       n = Math.floor(n /10);
-     }
-    return sum;
+    return n.toString().split('').reduce((acc, cur) => acc + Number(cur), 0);
 }
-
