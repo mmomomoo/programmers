abb235b262cca978511d1af673b595031c9d78ea
momomoo <164153608+mmomomoo@users.noreply.github.com>
2024-09-25 23:00:34 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,11 +1,5 @@
-
-const solution = (s1, s2) => {
-    let commonCount = 0; 
-    const setS2 = new Set(s2);
-    s1.forEach(el => {
-        if(setS2.has(el)){
-            commonCount++;
-        }
-    })
-    return commonCount;
-};
+function solution(s1, s2) {
+    const commonElements = s1.filter(element => s2.includes(element));
+    
+    return commonElements.length;
+}
