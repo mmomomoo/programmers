699d4b1ecdc81c93e4b27492be174516d7b6b044
momomoo <164153608+mmomomoo@users.noreply.github.com>
2024-10-01 23:13:18 +0900

    [level 0] Title: 약수 구하기, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,9 @@
-function solution (n) {
-    let divisors = [];
-    for(i = 1; i <= n; i++) {
+function solution(n) {
+    let answer = [];
+    for(let i = 1; i <= n; i++){
         if(n % i === 0){
-            divisors.push(i);
+            answer.push(i);
         }
     }
-    return divisors;
+    return answer;
 }
