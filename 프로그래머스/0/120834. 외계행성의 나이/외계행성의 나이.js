709def7f1f7f805fd58eec93f654fc3ca835d407
momomoo <164153608+mmomomoo@users.noreply.github.com>
2024-10-17 16:33:14 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,20 @@
+const alphabetByNumber ={
+    0 : 'a',
+    1 : 'b',
+    2 : 'c',
+    3 : 'd',
+    4 : 'e',
+    5 : 'f',
+    6 : 'g',
+    7 : 'h',
+    8 : 'i',
+    9 : 'j',
+}
+
+function solution(age) {
+    return String(age)
+          .split('')
+          .map(num => alphabetByNumber[num])
+          .join('');
+}
+
