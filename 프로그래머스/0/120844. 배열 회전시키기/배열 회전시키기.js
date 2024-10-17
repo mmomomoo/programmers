b83863cb4d17aa7442b861a0e1f08758f1312e02
momomoo <164153608+mmomomoo@users.noreply.github.com>
2024-10-17 17:08:49 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,10 +1,10 @@
 function solution(numbers, direction) {
-    if (direction === 'left') {
-        let firstElement = numbers.shift(); 
-        numbers.push(firstElement);
-    } else if (direction === 'right') {
-        let lastElement = numbers.pop(); 
-        numbers.unshift(lastElement);
+    if(direction === 'right'){
+       const last = numbers.pop();
+       numbers.unshift(last);       
+    } else if(direction === 'left'){
+       const first = numbers.shift();
+       numbers.push(first);
     }
     return numbers;
 }
